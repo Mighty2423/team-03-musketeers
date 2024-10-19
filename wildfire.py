61a4532bbc28f77bdd676cdfc92fc34de7699690
Joe0lsen <olsen.jcm@gmail.com>
2024-10-18 21:22:41 -0600

    Add wildfire choices

--- wildfire.py
+++ wildfire.py
@@ -1,6 +1,13 @@
 
-choices = {
+wildfire = {
         'intro': "You've been notified of a wildfire approaching in your area",
-        'intro_choices': ['Stay put', 'Get closer to water']
+        'at home': ['Stay put', 'Get closer to water','Leave and follow the evacution of the neighborhood'],
+        'middle of the forest': ['Follow large Wild life', 'look for water source or body of water','Start walking away and look for emergency'],
+        "middle of a lake": ['Pick up any one by the beaches', 'stay in the middle of the lake','Use a sigmal flare to call for help'],
+        'by a river': ['keep following the river down back to town', 'knee deep walking to the town'],#come back too
+        'cliff Side': ['Jump to the bottom','keep follon within edge until you find a lower area','start walking towards the forest.'],
+        'on the road': ['turn aaround back to the other town', 'head down to find any one needing a ride','','' ],
+        'by train tracks': ['follow your way back to home' ,'follow the way back to town','go in to the forest'],
+        'in a cornfield': ['get out to the road and start funning back','found a shovel and start digging a line',''],
 }
 
