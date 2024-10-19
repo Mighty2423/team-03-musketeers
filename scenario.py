3eb077a4d947df31835caef6d7c56d2c802b3673
Joe0lsen <olsen.jcm@gmail.com>
2024-10-18 21:22:58 -0600

    Add Scenario class

---
+++ scenario.py
@@ -0,0 +1,10 @@
+class Scenario:
+  def __init__(self, scenario):
+    self.scenario = scenario
+    self.messages = {
+            'end': "Thank you for playing!",
+            'error': "Invalid response. Please try again.",
+            'call911': 'Call 911',
+           }
+    
+    
