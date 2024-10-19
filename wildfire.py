decd7d7c12afad2286f7b0c733c4bc362bbc8e5b
Joe0lsen <olsen.jcm@gmail.com>
2024-10-19 02:38:34 -0600

    Tweaks to wildfire

--- wildfire.py
+++ wildfire.py
@@ -12,7 +12,7 @@ wildfire = {
 
 
 
-        'Go into forest': {"You go into the forest and get to the middle of the forest."},
+        'Go into forest': {'message': "You go into the forest and get to the middle of the forest."},
         'Follow wildlife': {'message': "You follow the large wild life and get chased by a bear. You die."},
         'Climb a tree': {'message': "You climb a tree and wait for help. The fire overtakes you. You die."},
         'Look for emergency personnel': {'message': "You start walking away and look for emergency personnel. You get lost and overtaken by the fire. You die."},
@@ -24,56 +24,48 @@ wildfire = {
         'Drive to nearest body of water': {'message': "You arrive at a body of water and get on a boat to middle of a lake."},
         'Find people to pickup': {'message': "You head down to find any one needing a ride"},
         'Leave car and run away': {'message': "You leave the car and start running away from the fire, but it overtakes you."},
-        #start a diagram for the other senario or hold for a bit? Ok  <-yes ill be in the flood since it will mostly the same as the wildfire    Sweet sound good
-        #really gotta get this one done first so the other guys and do something besides watch videos on youtube xD
-        #Actually, I'll try and wrap this one up if you want to start the next scenario ^^
-
-        # 'end conditions': {'You die',
-        #                    'You got away safely',
-        #                    'You got to the hospital',
-        #                    'You got to the police station and stayed and helped',
-        #                    'made it to the town',
-        #                    'found a shelter safe area'}
-        #                    # Maybe we just put these in the 'message' keys ... <yes that sounds like a better idea> sweet
 
 }
 
 
 #1
-wildfire['At home'['Get in a car']] = wildfire['Get in a car']
+wildfire['At home']['Get in a car'] = wildfire['Get in a car']
   #2
-wildfire['Get in a car'['Follow the evacuation']] = wildfire['Follow the evacuation']
+wildfire['Get in a car']['Follow the evacuation'] = wildfire['Follow the evacuation']
     #3
-wildfire['Follow the evacuation'['Leave car and run away']] = wildfire['Leave car and run away']
+wildfire['Follow the evacuation']['Leave car and run away'] = wildfire['Leave car and run away']
     #3
-wildfire['Follow the evacuation'['Find people to pickup']] = wildfire['Find people to pickup']
+wildfire['Follow the evacuation']['Find people to pickup'] = wildfire['Find people to pickup']
   #2
-wildfire['Get in a car'['Drive to nearest town']] = wildfire['Drive to nearest town']
+wildfire['Get in a car']['Drive to nearest town'] = wildfire['Drive to nearest town']
   #2
-wildfire['Get in a car'['Drive to nearest body of water']] = wildfire['Drive to nearest body of water']
+wildfire['Get in a car']['Drive to nearest body of water'] = wildfire['Drive to nearest body of water']
     #3
-wildfire['Drive to nearest body of water'['Pick people up in boat']] = wildfire['Pick people up in boat']
+wildfire['Drive to nearest body of water']['Pick people up in boat'] = wildfire['Pick people up in boat']
     #3
-wildfire['Drive to nearest body of water'['Stay in the lake']] = wildfire['Stay in the lake']
+wildfire['Drive to nearest body of water']['Stay in the lake'] = wildfire['Stay in the lake']
 
 
 #1
-wildfire['At home'['Stay put']] = wildfire['Stay put']
+wildfire['At home']['Stay put'] = wildfire['Stay put']
 
 
 #1
-wildfire['At home'['Go into forest']] = wildfire['Go into forest']
+wildfire['At home']['Go into forest'] = wildfire['Go into forest']
   #2
-wildfire['Go into forest'['Follow wildlife']] = wildfire['Follow wildlife']
+wildfire['Go into forest']['Follow wildlife'] = wildfire['Follow wildlife']
   #2
-wildfire['Go into forest'['Climb a tree']] = wildfire['Climb a tree']
+wildfire['Go into forest']['Climb a tree'] = wildfire['Climb a tree']
   #2
-wildfire['Go into forest'['Look for emergency personnel']] = wildfire['Look for emergency personnel']
+wildfire['Go into forest']['Look for emergency personnel'] = wildfire['Look for emergency personnel']
 
 
 #1
-wildfire['At home'['Get close to water']] = wildfire['Get close to water']
+wildfire['At home']['Get close to water'] = wildfire['Get close to water']
   #2
-wildfire['Get close to water'['Pick people up in boat']] = wildfire['Pick people up in boat']
+wildfire['Get close to water']['Pick people up in boat'] = wildfire['Pick people up in boat']
   #2
-wildfire['Get close to water'['Stay in the lake']] = wildfire['Stay in the lake']
+wildfire['Get close to water']['Stay in the lake'] = wildfire['Stay in the lake']
+
+def get():
+  return wildfire
